e6a3dbf2e4ddb638233bea559dbda2502875dcce
jamppa2 <j.mannynsalo@gmail.com>
2026-02-20 13:46:50 +0200

    Add files via upload

---
+++ OlioViikko7_tehtava_2/mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+   void setEditNum(int num);
+
+private slots:
+    void on_plusButton_clicked();
+
+    void on_miinusButton_clicked();
+
+    void on_yksiButton_clicked();
+
+    void on_kaksiButton_clicked();
+
+    void on_kolmeButton_clicked();
+
+    void on_enterButton_clicked();
+
+    void on_clearButton_clicked();
+
+    void on_neljaButton_clicked();
+
+    void on_viisiButton_clicked();
+
+    void on_kuusiButton_clicked();
+
+    void on_seitsemanButton_clicked();
+
+    void on_kahdeksanButton_clicked();
+
+    void on_yhdeksanButton_clicked();
+
+    void on_nollaButton_clicked();
+
+    void on_kertoButton_clicked();
+
+    void on_jakoButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    int num1;
+    int num2;
+    int num3;
+    int result;
+
+    int tila = 0;
+};
+#endif // MAINWINDOW_H
